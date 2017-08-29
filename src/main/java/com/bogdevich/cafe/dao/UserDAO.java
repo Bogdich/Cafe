b5e4214e6822f86c5eff60cb3062c722f8b9418e
eneguE <bogdevich96@hotmail.com>
2017-08-30 02:33:00 +0300

    return with Optional<T> value

---
+++ src/main/java/com/bogdevich/cafe/dao/UserDAO.java
@@ -0,0 +1,16 @@
+package com.bogdevich.cafe.dao;
+
+import com.bogdevich.cafe.entity.bean.User;
+import com.bogdevich.cafe.exception.DAOException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDAO{
+    List<User> findAll() throws DAOException;
+    Optional<User> create(String login, String email, String password, int roleID) throws DAOException;
+    Optional<User> findUserByLogin(String login) throws DAOException;
+    Optional<User> findUserById(int id) throws DAOException;
+    boolean update(User user) throws DAOException;
+    boolean delete(User user) throws DAOException;
+}
