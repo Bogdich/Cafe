5a4114568472cc4a8264fe04e15aed24516bbb1a
eneguE <bogdevich96@hotmail.com>
2017-09-05 15:42:11 +0300

    Creating userDAO interface

--- src/main/java/com/bogdevich/cafe/dao/UserDAO.java
+++ src/main/java/com/bogdevich/cafe/dao/UserDAO.java
@@ -1,17 +1,24 @@
 package com.bogdevich.cafe.dao;
 
 import com.bogdevich.cafe.entity.bean.User;
-import com.bogdevich.cafe.entity.enums.Role;
+import com.bogdevich.cafe.entity.type.Role;
 import com.bogdevich.cafe.exception.DAOException;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserDAO{
+public interface UserDAO {
+    Optional<User> create(int number, String password, String name, BigDecimal balance, Role role) throws DAOException;
+
     List<User> findAll() throws DAOException;
-    Optional<User> create(String login, String email, String password, Role role) throws DAOException;
-    Optional<User> findUserByLogin(String login) throws DAOException;
+    Optional<User> findUserByNumber(int number) throws DAOException;
     Optional<User> findUserById(int id) throws DAOException;
+
     boolean update(User user) throws DAOException;
+    boolean updateBalance(BigDecimal balance) throws DAOException;
+    boolean updatePassword(String password) throws DAOException;
+    boolean updateNumber(int number) throws DAOException;
+
     boolean delete(User user) throws DAOException;
 }
