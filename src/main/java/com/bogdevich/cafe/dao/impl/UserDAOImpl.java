dad14416601f04296055fc3711fc0682094b1e7b
eneguE <bogdevich96@hotmail.com>
2017-09-06 02:55:41 +0300

    Adding UserDAO and UserInfoDAO implementations. Methods refactoring.

--- src/main/java/com/bogdevich/cafe/dao/impl/UserDAOImpl.java
+++ src/main/java/com/bogdevich/cafe/dao/impl/UserDAOImpl.java
@@ -20,15 +20,11 @@ import static com.bogdevich.cafe.constant.RBErrorMessage.DAOMessage.*;
 public class UserDAOImpl implements UserDAO {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private static final String SQL_FIND_ALL = new StringBuilder()
-            .append("SELECT ")
-            .append("`id`, `number`, `password`, `name`, `role_id` ")
-            .append("FROM user")
-            .toString();
-    private static final String SQL_CREATE = "INSERT INTO user(`number`, `password`, `name`, `role_id`) VALUES(?,?,?,?)";
-    private static final String SQL_FIND_BY_NUMBER = "SELECT `id`, `number`, `password`, `name`, `role_id` FROM user WHERE `number`=?";
-    private static final String SQL_FIND_BY_ID = "SELECT `id`, `number`, `password`, `name`, `role_id` FROM user WHERE `id`=?";
-    private static final String SQL_UPDATE_USER = "UPDATE user SET `number`=?, `password`=?, `name`=? WHERE `id`=?";
+    private static final String SQL_SELECT_ALL_USERS = "SELECT `id`, `login`, `password`, `account_balance`, `role_id` FROM user";
+    private static final String SQL_CREATE = "INSERT INTO user(`login`, `password`, `account_balance`, `role_id`) VALUES(?,?,?,?)";
+    private static final String SQL_SELECT_BY_LOGIN = "SELECT `id`, `login`, `password`, `account_balance`, `role_id` FROM user WHERE `login`=?";
+    private static final String SQL_SELECT_BY_ID = "SELECT `id`, `login`, `password`, `account_balance`, `role_id` FROM user WHERE `id`=?";
+    private static final String SQL_UPDATE_USER = "UPDATE user SET `login`=?, `password`=?, `account_balance`=?, `role_id`=? WHERE `id`=?";
     private static final String SQL_DELETE_BY_NUMBER = "DELETE FROM user WHERE `number`=?";
 
     private final IDataSource dataSource;
@@ -38,36 +34,20 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public List<User> findAll() throws DAOException {
-        ArrayList<User> users = new ArrayList<>();
-        Connection connection = dataSource.getConnection();
-        try (PreparedStatement statement = connection.prepareStatement(SQL_FIND_ALL)) {
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                users.add(insertData(resultSet));
-            }
-        } catch (SQLException ex) {
-            LOGGER.log(Level.ERROR, ex.getMessage());
-            throw new DAOException(USER_FIND_ALL, ex);
-        }
-        return users;
-    }
-
-    @Override
-    public Optional<User> create(int number, String password, String name, BigDecimal price,Role role) throws DAOException {
+    public Optional<User> create(String login, String password, BigDecimal balance, Role role) throws DAOException {
         User user;
         Connection connection = dataSource.getConnection();
         try (PreparedStatement statement = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS)) {
-            statement.setInt(1, number);
+            statement.setString(1, login);
             statement.setString(2, password);
-            statement.setString(3, name);
+            statement.setBigDecimal(3, balance);
             statement.setInt(4, role.getId());
 
             int rowsAffected = statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
 
             if (resultSet.next() && rowsAffected == 1) {
-                user = new User(number, password, name, price, role);
+                user = new User(login, password, role, balance);
                 user.setId(resultSet.getInt(1));
             } else {
                 LOGGER.log(Level.WARN, "Can not create user");
@@ -80,11 +60,27 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public Optional<User> findUserByNumber(int number) throws DAOException {
+    public List<User> findAll() throws DAOException {
+        ArrayList<User> users = new ArrayList<>();
+        Connection connection = dataSource.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_ALL_USERS)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                users.add(insertData(resultSet));
+            }
+        } catch (SQLException ex) {
+            LOGGER.log(Level.ERROR, ex.getMessage());
+            throw new DAOException(USER_FIND_ALL, ex);
+        }
+        return users;
+    }
+
+    @Override
+    public Optional<User> findUserByLogin(String login) throws DAOException {
         Connection connection = dataSource.getConnection();
         User user = null;
-        try (PreparedStatement statement = connection.prepareStatement(SQL_FIND_BY_NUMBER)) {
-            statement.setInt(1, number);
+        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_BY_LOGIN)) {
+            statement.setString(1, login);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 user = insertData(resultSet);
@@ -99,7 +95,7 @@ public class UserDAOImpl implements UserDAO {
     public Optional<User> findUserById(int id) throws DAOException {
         Connection connection = dataSource.getConnection();
         User user = null;
-        try (PreparedStatement statement = connection.prepareStatement(SQL_FIND_BY_ID)) {
+        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_BY_ID)) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
@@ -115,10 +111,11 @@ public class UserDAOImpl implements UserDAO {
     public boolean update(User user) throws DAOException {
         Connection connection = dataSource.getConnection();
         try (PreparedStatement statement = connection.prepareStatement(SQL_UPDATE_USER)) {
-            statement.setInt(1, user.getNumber());
+            statement.setString(1, user.getLogin());
             statement.setString(2, user.getPassword());
-            statement.setString(3, user.getName());
-            statement.setInt(4, user.getId());
+            statement.setBigDecimal(3, user.getAccountBalance());
+            statement.setInt(4, user.getRole().getId());
+            statement.setInt(5, user.getId());
             int rowsAffected = statement.executeUpdate();
             return (rowsAffected==1);
         } catch (SQLException ex) {
@@ -137,7 +134,7 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public boolean updateNumber(int number) throws DAOException {
+    public boolean updateLogin(String login) throws DAOException {
         throw new UnsupportedOperationException();
     }
 
@@ -145,7 +142,7 @@ public class UserDAOImpl implements UserDAO {
     public boolean delete(User user) throws DAOException {
         Connection connection = dataSource.getConnection();
         try (PreparedStatement statement = connection.prepareStatement(SQL_DELETE_BY_NUMBER)) {
-            statement.setInt(1, user.getNumber());
+            statement.setString(1, user.getLogin());
             int rowsAffected = statement.executeUpdate();
             return (rowsAffected==1);
         }catch (SQLException ex) {
@@ -156,9 +153,9 @@ public class UserDAOImpl implements UserDAO {
     private User insertData(ResultSet resultSet) throws SQLException {
         User user = new User();
         user.setId(resultSet.getInt("id"));
-        user.setNumber(resultSet.getInt("number"));
-        user.setName(resultSet.getString("name"));
+        user.setLogin(resultSet.getString("login"));
         user.setPassword(resultSet.getString("password"));
+        user.setAccountBalance(resultSet.getBigDecimal("account_balance"));
         user.setRole(Role.getRoleById(resultSet.getInt("role_id")));
         return user;
     }
---
+++ src/main/java/com/bogdevich/cafe/dao/impl/UserInfoDAOImpl.java
@@ -0,0 +1,150 @@
+package com.bogdevich.cafe.dao.impl;
+
+import com.bogdevich.cafe.dao.UserInfoDAO;
+import com.bogdevich.cafe.entity.bean.UserInfo;
+import com.bogdevich.cafe.exception.DAOException;
+import com.bogdevich.cafe.transaction.IDataSource;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.bogdevich.cafe.constant.RBErrorMessage.DAOMessage.*;
+
+public class UserInfoDAOImpl implements UserInfoDAO {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final String SQL_INSERT = "INSERT INTO user_info (`number`, `street`, `house_number`, `flat`, `user_id`) VALUES (?, ?, ?, ?, ?)";
+    private static final String SQL_SELECT_ALL = "SELECT `id`, `number`, `street`, `house_number`, `flat`, `user_id` FROM user_info";
+    private static final String SQL_SELECT_BY_ID = "SELECT `id`,`number`, `street`, `house_number`, `flat`, `user_id` FROM user_info WHERE `id`=?";
+    private static final String SQL_SELECT_BY_USER_ID = "SELECT `id`, `number`, `street`, `house_number`, `flat`, `user_id` FROM user_info WHERE `user_id`=?";
+    /**
+     * Not done
+     */
+    private static final String SQL_SELECT_BY_ORDER_ID = "SELECT `id`, `number`, `street`, `house_number`, `flat`, `user_id` FROM user_info ";
+    private static final String SQL_UPDATE = "UPDATE user_info SET `number`=?, `street`=?, `house_number`?, `flat`? WHERE `id`=?";
+    private static final String SQL_DELETE = "DELETE FROM address WHERE `id`=?";
+
+    private final IDataSource dataSource;
+
+    public UserInfoDAOImpl(IDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Optional<UserInfo> create(int number, String street, String house, int flat, int userID) throws DAOException {
+        UserInfo userinfo;
+        Connection connection = dataSource.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, number);
+            statement.setString(2, street);
+            statement.setString(3, house);
+            statement.setInt(4, flat);
+            statement.setInt(5, userID);
+
+            int rowsAffected = statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+
+            if (resultSet.next() && rowsAffected == 1) {
+                userinfo = new UserInfo(number, street, house, flat, userID);
+                userinfo.setId(resultSet.getInt(1));
+            } else {
+                LOGGER.log(Level.WARN, "Can not create userinfo");
+                throw new DAOException();
+            }
+            return Optional.ofNullable(userinfo);
+        } catch (SQLException ex) {
+            throw new DAOException(ex);
+        }
+    }
+
+    @Override
+    public List<UserInfo> findAll() throws DAOException {
+        ArrayList<UserInfo> userinfos = new ArrayList<>();
+        Connection connection = dataSource.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_ALL)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                userinfos.add(insertData(resultSet));
+            }
+        } catch (SQLException ex) {
+            LOGGER.log(Level.ERROR, ex.getMessage());
+            throw new DAOException(ADDRES_FIND_ALL, ex);
+        }
+        return userinfos;
+    }
+
+    @Override
+    public List<UserInfo> findUserInfoByUserId(int userID) throws DAOException {
+        Connection connection = dataSource.getConnection();
+        ArrayList<UserInfo> userinfos = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_BY_USER_ID)) {
+            statement.setInt(1, userID);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                userinfos.add(insertData(resultSet));
+            }
+            return userinfos;
+        } catch (SQLException ex) {
+            throw new DAOException(ADDRESS_FIND_BY, ex);
+        }
+    }
+
+    @Override
+    public Optional<UserInfo> findUserInfoByOrderId(int orderID) throws DAOException {
+//        Connection connection = dataSource.getConnection();
+//        UserInfo userinfo = null;
+//        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_BY_ORDER_ID)) {
+//            statement.setInt(1, orderID);
+//            ResultSet resultSet = statement.executeQuery();
+//            if (resultSet.next()) {
+//                userinfo = insertData(resultSet);
+//            }
+//            return Optional.ofNullable(userinfo);
+//        } catch (SQLException ex) {
+//            throw new DAOException(USER_FIND_BY_ID, ex);
+//        }
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<UserInfo> findUserInfoById(int id) throws DAOException {
+        Connection connection = dataSource.getConnection();
+        UserInfo userInfo = null;
+        try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT_BY_ID)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                userInfo = insertData(resultSet);
+            }
+            return Optional.ofNullable(userInfo);
+        } catch (SQLException ex) {
+            throw new DAOException(USER_FIND_BY_ID, ex);
+        }
+    }
+
+    @Override
+    public boolean update(UserInfo userinfo) throws DAOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean delete(UserInfo userinfo) throws DAOException {
+        throw new UnsupportedOperationException();
+    }
+
+    private UserInfo insertData(ResultSet resultSet) throws SQLException {
+        UserInfo userinfo = new UserInfo();
+        userinfo.setId(resultSet.getInt("id"));
+        userinfo.setNumber(resultSet.getInt("number"));
+        userinfo.setStreet(resultSet.getString("street"));
+        userinfo.setHouse(resultSet.getString("house_number"));
+        userinfo.setFlat(resultSet.getInt("flat"));
+        userinfo.setUserID(resultSet.getInt("user_id"));
+        return userinfo;
+    }
+}
