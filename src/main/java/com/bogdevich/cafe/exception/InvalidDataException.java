016f63bf66792d6406d550ac391a7dac1f042702
eneguE <bogdevich96@hotmail.com>
2017-09-12 17:22:00 +0300

    Creating new type of Exception

---
+++ src/main/java/com/bogdevich/cafe/exception/InvalidDataException.java
@@ -0,0 +1,20 @@
+package com.bogdevich.cafe.exception;
+
+public class InvalidDataException extends Exception {
+
+    public InvalidDataException() {
+        super();
+    }
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+
+    public InvalidDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidDataException(Throwable cause) {
+        super(cause);
+    }
+}
