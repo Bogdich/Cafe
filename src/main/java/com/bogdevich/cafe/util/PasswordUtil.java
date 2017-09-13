b06b81433e3c0a8dd1f73619e24662a0eee9999e
eneguE <bogdevich96@hotmail.com>
2017-09-13 05:37:01 +0300

    Adding java doc. Making class final.

--- src/main/java/com/bogdevich/cafe/util/PasswordUtil.java
+++ src/main/java/com/bogdevich/cafe/util/PasswordUtil.java
@@ -1,15 +1,15 @@
-package com.bogdevich.cafe.service.util;
+package com.bogdevich.cafe.util;
 
 import static org.mindrot.jbcrypt.BCrypt.*;
 
-public class PasswordUtil {
+public final class PasswordUtil {
     /**
      * Define the BCrypt WORKLOAD to use when generating password hashes. Minimum 4, maximum 31.
      */
     private final static int WORKLOAD = 12;
 
     /**
-     * All of hashed strings start with this value.
+     * All of hashed strings starts with this value.
      */
     private final static String HASH_START = "$2a$";
 
@@ -20,6 +20,15 @@ public class PasswordUtil {
         return hashpw(password, gensalt(WORKLOAD));
     }
 
+    /**
+     *
+     * @param password a password to be compared with <code>storedHash</code>
+     * @param storedHash a value stored in database
+     * @return <code>true</code> if <code>storedHash</code> equals to <code>password</code>
+     * by bcrypt algorithm
+     * @throws IllegalArgumentException if <code>storedHash</code> is null
+     * or doesn't start with <code>HASH_START</code>
+     */
     public static boolean checkPassword(String password, String storedHash) throws IllegalArgumentException {
         if (null == storedHash || !storedHash.startsWith(HASH_START)) {
             throw new IllegalArgumentException("Invalid hash is provided for comparison");
