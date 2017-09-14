267635308d08be38a946d5c0ac7f93971ec92951
eneguE <bogdevich96@hotmail.com>
2017-09-14 15:07:44 +0300

    Creating main Servlet

---
+++ src/main/java/com/bogdevich/cafe/controller/servlet/FrontController.java
@@ -0,0 +1,36 @@
+package com.bogdevich.cafe.controller.servlet;
+
+import com.bogdevich.cafe.command.ActionType;
+import com.bogdevich.cafe.constant.Constant;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "FrontController", urlPatterns = "/controller")
+public class FrontController extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger(FrontController.class);
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.process(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String actionName = request.getParameter(Constant.ParameterName.COMMAND);
+        ActionType actionType = ActionType.defineActionType(actionName);
+        if (null != actionType) {
+            actionType.getCommand().execute(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+}
