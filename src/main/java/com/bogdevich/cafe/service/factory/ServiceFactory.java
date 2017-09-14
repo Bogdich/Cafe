ccdce2a9641938ed4ad5f34be5d81e0264cbf2b0
eneguE <bogdevich96@hotmail.com>
2017-09-14 15:14:11 +0300

    Renaming to ServiceFactory

--- src/main/java/com/bogdevich/cafe/service/factory/ServiceFactory.java
+++ src/main/java/com/bogdevich/cafe/service/factory/ServiceFactory.java
@@ -5,15 +5,15 @@ import com.bogdevich.cafe.service.impl.UserServiceImpl;
 import com.bogdevich.cafe.transaction.ITransactionManager;
 import com.bogdevich.cafe.transaction.impl.TransactionManager;
 
-public class ReceiverFactory {
-    private static ReceiverFactory instance = new ReceiverFactory();
+public class ServiceFactory {
+    private static ServiceFactory instance = new ServiceFactory();
 
     private final UserService userService = new UserServiceImpl((ITransactionManager) TransactionManager.getInstance());
 
-    private ReceiverFactory() {
+    private ServiceFactory() {
     }
 
-    public static ReceiverFactory getInstance() {
+    public static ServiceFactory getInstance() {
         return instance;
     }
 
