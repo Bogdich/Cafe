5013e00ef86610ddf98b74c42e267931aa123898
Eugene Bogdevich <bogdevich96@gmail.com>
2017-09-19 19:57:04 +0300

    Adding method for defining a dish category by its id

--- src/main/java/com/bogdevich/cafe/entity/type/Category.java
+++ src/main/java/com/bogdevich/cafe/entity/type/Category.java
@@ -1,10 +1,29 @@
 package com.bogdevich.cafe.entity.type;
 
 public enum Category {
-    TWISTER,
-    BURGER,
-    PANCAKE,
-    DRINK,
-    OTHER;
+    TWISTER(1),
+    BURGER(2),
+    PANCAKE(3),
+    DRINK(4),
+    OTHER(5);
 
+    private int id;
+
+    Category(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public static Category defineCategory(int id) {
+        for (Category category :
+                Category.values()) {
+            if (category.id == id) {
+                return category;
+            }
+        }
+        return null;
+    }
 }
