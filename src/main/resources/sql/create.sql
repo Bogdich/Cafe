d51b21019507f076f097a93824b503990fa02276
Eugene Bogdevich <bogdevich96@gmail.com>
2017-09-19 14:55:57 +0300

    Database reengineering

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema cafe
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `cafe` ;
@@ -16,75 +19,17 @@ CREATE SCHEMA IF NOT EXISTS `cafe` DEFAULT CHARACTER SET utf8 ;
 USE `cafe` ;
 
 -- -----------------------------------------------------
--- Table `cafe`.`role`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cafe`.`role` ;
-
-CREATE TABLE IF NOT EXISTS `cafe`.`role` (
-  `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-  ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cafe`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cafe`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `cafe`.`user` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(150) NOT NULL,
-  `password` VARCHAR(150) NOT NULL,
-  `account_balance` DECIMAL(5,2) NOT NULL DEFAULT 0,
-  `create_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `role_id` TINYINT UNSIGNED NOT NULL DEFAULT 2,
-  PRIMARY KEY (`id`),
-  INDEX `fk_user_role_idx` (`role_id` ASC),
-  UNIQUE INDEX `login_UNIQUE` (`login` ASC),
-  CONSTRAINT `fk_user_role`
-  FOREIGN KEY (`role_id`)
-  REFERENCES `cafe`.`role` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 1;
-
-
--- -----------------------------------------------------
--- Table `cafe`.`user_info`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cafe`.`user_info` ;
-
-CREATE TABLE IF NOT EXISTS `cafe`.`user_info` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `number` INT(9) UNSIGNED ZEROFILL NOT NULL,
-  `street` VARCHAR(150) NOT NULL,
-  `house_number` VARCHAR(10) NOT NULL,
-  `flat` SMALLINT UNSIGNED NOT NULL,
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_address_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_address_user1`
-  FOREIGN KEY (`user_id`)
-  REFERENCES `cafe`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 1;
-
-
--- -----------------------------------------------------
 -- Table `cafe`.`category`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `cafe`.`category` ;
 
 CREATE TABLE IF NOT EXISTS `cafe`.`category` (
-  `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
+  `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(150) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC)  COMMENT '')
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -93,22 +38,60 @@ CREATE TABLE IF NOT EXISTS `cafe`.`category` (
 DROP TABLE IF EXISTS `cafe`.`dish` ;
 
 CREATE TABLE IF NOT EXISTS `cafe`.`dish` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(150) NOT NULL,
-  `price` DECIMAL(5,2) NOT NULL,
-  `weight` SMALLINT NOT NULL,
-  `description` VARCHAR(255) NOT NULL,
-  `picture_path` VARCHAR(150) NULL,
-  `category_id` TINYINT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_dish_category1_idx` (`category_id` ASC),
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(150) NOT NULL COMMENT '',
+  `price` DECIMAL(5,2) NOT NULL COMMENT '',
+  `weight` SMALLINT NOT NULL COMMENT '',
+  `description` TINYTEXT NOT NULL COMMENT '',
+  `picture_path` VARCHAR(150) NULL COMMENT '',
+  `category_id` TINYINT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `fk_dish_category1_idx` (`category_id` ASC)  COMMENT '',
   CONSTRAINT `fk_dish_category1`
   FOREIGN KEY (`category_id`)
   REFERENCES `cafe`.`category` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
   ENGINE = InnoDB
-  AUTO_INCREMENT = 1;
+  DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `cafe`.`role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cafe`.`role` ;
+
+CREATE TABLE IF NOT EXISTS `cafe`.`role` (
+  `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(150) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC)  COMMENT '')
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `cafe`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cafe`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `cafe`.`user` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `login` VARCHAR(150) NOT NULL COMMENT '',
+  `password` VARCHAR(150) NOT NULL COMMENT '',
+  `account_balance` DECIMAL(5,2) NOT NULL DEFAULT '0.00' COMMENT '',
+  `create_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `role_id` TINYINT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC)  COMMENT '',
+  INDEX `fk_user_role1_idx` (`role_id` ASC)  COMMENT '',
+  CONSTRAINT `fk_user_role1`
+  FOREIGN KEY (`role_id`)
+  REFERENCES `cafe`.`role` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -117,22 +100,46 @@ CREATE TABLE IF NOT EXISTS `cafe`.`dish` (
 DROP TABLE IF EXISTS `cafe`.`order` ;
 
 CREATE TABLE IF NOT EXISTS `cafe`.`order` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `total_price` DECIMAL(5,2) NULL,
-  `create_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  `number` INT(9) UNSIGNED ZEROFILL NOT NULL,
-  `street` VARCHAR(150) NOT NULL,
-  `house_number` VARCHAR(10) NOT NULL,
-  `flat` SMALLINT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_order_user1_idx` (`user_id` ASC),
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `total_price` DECIMAL(5,2) NULL COMMENT '',
+  `create_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `number` INT UNSIGNED ZEROFILL NOT NULL COMMENT '',
+  `street` VARCHAR(150) NOT NULL COMMENT '',
+  `house_number` VARCHAR(10) NOT NULL COMMENT '',
+  `flat` SMALLINT UNSIGNED NOT NULL COMMENT '',
+  `user_id` INT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `fk_order_user1_idx` (`user_id` ASC)  COMMENT '',
   CONSTRAINT `fk_order_user1`
   FOREIGN KEY (`user_id`)
   REFERENCES `cafe`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-  ENGINE = InnoDB;
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `cafe`.`user_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cafe`.`user_info` ;
+
+CREATE TABLE IF NOT EXISTS `cafe`.`user_info` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `number` INT UNSIGNED ZEROFILL NOT NULL COMMENT '',
+  `street` VARCHAR(150) NOT NULL COMMENT '',
+  `house_number` VARCHAR(10) NOT NULL COMMENT '',
+  `flat` SMALLINT UNSIGNED NOT NULL COMMENT '',
+  `user_id` INT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `fk_user_info_user1_idx` (`user_id` ASC)  COMMENT '',
+  CONSTRAINT `fk_user_info_user1`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `cafe`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -141,12 +148,12 @@ CREATE TABLE IF NOT EXISTS `cafe`.`order` (
 DROP TABLE IF EXISTS `cafe`.`order_has_dish` ;
 
 CREATE TABLE IF NOT EXISTS `cafe`.`order_has_dish` (
-  `order_id` INT(10) UNSIGNED NOT NULL,
-  `dish_id` INT(10) UNSIGNED NOT NULL,
-  `quantity` SMALLINT UNSIGNED NOT NULL DEFAULT 1,
-  PRIMARY KEY (`order_id`, `dish_id`),
-  INDEX `fk_order_has_dish_dish1_idx` (`dish_id` ASC),
-  INDEX `fk_order_has_dish_order1_idx` (`order_id` ASC),
+  `order_id` INT UNSIGNED NOT NULL COMMENT '',
+  `dish_id` INT UNSIGNED NOT NULL COMMENT '',
+  `quantity` TINYINT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`order_id`, `dish_id`)  COMMENT '',
+  INDEX `fk_order_has_dish_dish1_idx` (`dish_id` ASC)  COMMENT '',
+  INDEX `fk_order_has_dish_order1_idx` (`order_id` ASC)  COMMENT '',
   CONSTRAINT `fk_order_has_dish_order1`
   FOREIGN KEY (`order_id`)
   REFERENCES `cafe`.`order` (`id`)
@@ -157,7 +164,34 @@ CREATE TABLE IF NOT EXISTS `cafe`.`order_has_dish` (
   REFERENCES `cafe`.`dish` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-  ENGINE = InnoDB;
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `cafe`.`shopping_cart`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cafe`.`shopping_cart` ;
+
+CREATE TABLE IF NOT EXISTS `cafe`.`shopping_cart` (
+  `user_id` INT UNSIGNED NOT NULL COMMENT '',
+  `dish_id` INT UNSIGNED NOT NULL COMMENT '',
+  `quantity` TINYINT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`user_id`, `dish_id`)  COMMENT '',
+  INDEX `fk_user_has_dish_dish2_idx` (`dish_id` ASC)  COMMENT '',
+  INDEX `fk_user_has_dish_user2_idx` (`user_id` ASC)  COMMENT '',
+  CONSTRAINT `fk_user_has_dish_user2`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `cafe`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_dish_dish2`
+  FOREIGN KEY (`dish_id`)
+  REFERENCES `cafe`.`dish` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
