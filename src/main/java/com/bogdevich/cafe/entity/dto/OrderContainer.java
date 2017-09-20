0c78366b39bb51d0055df8205364f01eb60931e4
Eugene Bogdevich <bogdevich96@gmail.com>
2017-09-20 23:55:26 +0300

    Creating order container

---
+++ src/main/java/com/bogdevich/cafe/entity/dto/OrderContainer.java
@@ -0,0 +1,9 @@
+package com.bogdevich.cafe.entity.dto;
+
+import com.bogdevich.cafe.entity.bean.Order;
+import com.bogdevich.cafe.entity.bean.OrderDish;
+
+public class OrderContainer {
+    private Order order;
+    private OrderDish orderDish;
+}
